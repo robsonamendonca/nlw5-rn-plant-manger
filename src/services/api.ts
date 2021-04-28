878724226fdc2b2b7c67787baf0f137c587fd9a9
Robson Mendonça <rob.ams@gmail.com>
2021-04-27 21:04:14 -0300

    api my-json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.5:3333',
+    baseURL: 'https://my-json-server.typicode.com/robsonamendonca/api-plantmanager',
 
 });
 
